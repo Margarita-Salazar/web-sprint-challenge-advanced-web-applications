d0bcd345f74de02870ce425f1e88d6443708f902
unknown <margarita.salazar.mls@gmail.com>
2021-04-23 12:58:22 -0500

    made login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router";
+
+const initialValues = {
+  username: "", 
+  password: "",
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [ formValues, setFormValues ] = useState(initialValues)
+  const { history } = useHistory();
 
-  useEffect(()=>{
-    // make a post request to retrieve a token from the api
-    // when you have handled the token, navigate to the BubblePage route
-  });
+
+  // useEffect(()=>{
+  //   // make a post request to retrieve a token from the api
+  //   // when you have handled the token, navigate to the BubblePage route
+  //   axios.post("http://localhost:5000/api/login", formValues)
+  //     .then(res=>{
+  //       localStorage.setItem("token", res.data.payload)
+  //       history.push("/colors")
+  //     })
+  //     .catch(err=>{
+  //       console.log("error with login:", err.response)
+  //     });
+  // },[formValues]);
+
+  const submit = (e) => {
+    e.preventDefault()
+
+      // make a post request to retrieve a token from the api
+      // when you have handled the token, navigate to the BubblePage route
+      axios.post("http://localhost:5000/api/login", formValues)
+      .then(res=>{
+        localStorage.setItem("token", res.data.payload)
+        history.push("/colors")
+      })
+      .catch(err=>{
+        console.log("error with login:", err.response)
+      });
   
+    
+    setFormValues(initialValues);
+  }
+  
+  const change = (e) => {
+    setFormValues({...formValues, [e.target.name]: e.target.value})
+  }
+
   const error = "";
   //replace with error state
 
@@ -17,6 +57,28 @@ const Login = () => {
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
+      <form className="login-form" onSubmit={submit}>
+        <h1>Welcome to the Bubble App!</h1>
+        <label>
+          Username
+          <input 
+            type="text"
+            name="username"
+            value={formValues.username}
+            onChange={change}
+          />
+        </label>
+        <label>
+          Password
+          <input 
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={change}
+          />
+        </label>
+        <button>Sign in</button>
+      </form>
       </div>
 
       <p data-testid="errorMessage" className="error">{error}</p>
